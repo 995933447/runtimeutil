4df1cbbc09660c18889705a9276832bc06cfa772
995933447 <34621597+995933447@users.noreply.github.com>
2025-06-13 21:44:37 +0400

    Update stack_err_test.go

--- stack_err_test.go
+++ stack_err_test.go
@@ -7,8 +7,14 @@ import (
 
 func TestStackErr(t *testing.T) {
 	t.Log(testStackErr())
+	t.Log(testStackErrWithSkip(1))
+	t.Log(testStackErrWithSkip(2))
 }
 
 func testStackErr() error {
 	return NewStackErr(errors.New("test error"))
+}
+
+func testStackErrWithSkip(skip int) error {
+	return NewStackErrWithSkip(skip, errors.New("test error"))
 }
